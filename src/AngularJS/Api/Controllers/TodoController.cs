a8123cf291ae17401929e43b6fc58f1c7a496c0b
jbuisson <jbuisson@octo.com>
2014-05-26 19:53:40 +0200

    Cors

--- src/AngularJS/Api/Controllers/TodoController.cs
+++ src/AngularJS/Api/Controllers/TodoController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Api.Controllers
 {
+	[EnableCors("*", "*", "*")]
 	public class TodoController : ApiController
 	{
 		public IHttpActionResult Get()
